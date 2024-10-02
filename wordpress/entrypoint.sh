84ad0d255f5f368c6f119aea7155def75bd9bdef
almalinux <almalinux@localhost.localdomain>
2024-10-02 09:31:19 +0900

    fix autoupdate

--- wordpress/entrypoint.sh
+++ wordpress/entrypoint.sh
@@ -7,21 +7,14 @@ if [ ! -f /var/www/html/index.php ]; then
   tar -xzf /tmp/wordpress-4.7.1.tar.gz -C /var/www/html --strip-components=1
   chown -R www-data:www-data /var/www/html
   echo "WordPress has been extracted."
-fi
 
-# WordPress 自動アップデート抑止用の設定を wp-config.php に追加
-if [ -f /var/www/html/wp-config.php ]; then
-  if ! grep -q "define('AUTOMATIC_UPDATER_DISABLED', true);" /var/www/html/wp-config.php; then
-    echo "Disabling automatic updates in wp-config.php..."
-    echo "define('AUTOMATIC_UPDATER_DISABLED', true);" >> /var/www/html/wp-config.php
+  # wp-config-sample.php に自動アップデート抑止の設定を追加
+  if [ -f /var/www/html/wp-config-sample.php ]; then
+    echo "Modifying wp-config-sample.php to disable automatic updates..."
+    if ! grep -q "define('AUTOMATIC_UPDATER_DISABLED', true);" /var/www/html/wp-config-sample.php; then
+      echo "define('AUTOMATIC_UPDATER_DISABLED', true);" >> /var/www/html/wp-config-sample.php
+    fi
   fi
-else
-  echo "Creating wp-config.php with automatic updates disabled..."
-  cat <<EOL > /var/www/html/wp-config.php
-<?php
-define('AUTOMATIC_UPDATER_DISABLED', true);
-EOL
-  chown www-data:www-data /var/www/html/wp-config.php
 fi
 
 # Apache の起動
