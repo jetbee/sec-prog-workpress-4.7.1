a9de53fda1f8b92104fa846b46ff0fe364018499
almalinux <almalinux@localhost.localdomain>
2024-10-02 09:11:58 +0900

    fix autoupdate/mysql aarch64

--- wordpress/entrypoint.sh
+++ wordpress/entrypoint.sh
@@ -9,6 +9,21 @@ if [ ! -f /var/www/html/index.php ]; then
   echo "WordPress has been extracted."
 fi
 
+# WordPress 自動アップデート抑止用の設定を wp-config.php に追加
+if [ -f /var/www/html/wp-config.php ]; then
+  if ! grep -q "define('AUTOMATIC_UPDATER_DISABLED', true);" /var/www/html/wp-config.php; then
+    echo "Disabling automatic updates in wp-config.php..."
+    echo "define('AUTOMATIC_UPDATER_DISABLED', true);" >> /var/www/html/wp-config.php
+  fi
+else
+  echo "Creating wp-config.php with automatic updates disabled..."
+  cat <<EOL > /var/www/html/wp-config.php
+<?php
+define('AUTOMATIC_UPDATER_DISABLED', true);
+EOL
+  chown www-data:www-data /var/www/html/wp-config.php
+fi
+
 # Apache の起動
 apache2-foreground
 
